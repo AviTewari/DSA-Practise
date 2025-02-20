73bf8df2c6974b5f87f477574687865d06d95d4b
Avi Tewari <56482375+AviTewari@users.noreply.github.com>
2025-02-20 15:32:55 -0800

    Time: 179 ms (30.72%) | Memory: 18 MB (48.40%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -7,6 +7,6 @@ class Solution:
             count[s[r]] = 1 + count.get(s[r], 0)
             while (r-l+1) - max(count.values()) > k:
                 count[s[l]] -= 1
-                l+=1
+                l += 1
             res = max(res, r-l+1)
         return res
