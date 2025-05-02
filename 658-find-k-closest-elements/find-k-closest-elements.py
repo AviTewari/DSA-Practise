855f52c17c586fe87c451f693ceee98ca57c33cc
Avi Tewari <56482375+AviTewari@users.noreply.github.com>
2025-05-02 10:47:50 -0700

    Time: 7 ms (64.31%) | Memory: 19.2 MB (47.88%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -1,26 +1,20 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        l, r = 0, len(arr) - 1
-        
-        # Binary search to find the insert position of x
-        while l < r:
-            mid = (l + r) // 2
-            if arr[mid] < x:
-                l = mid + 1
+        l,r = 0,len(arr)-1
+        while l<r:
+            mid = (l+r)//2
+            if arr[mid] <x:
+                l = mid +1
             else:
                 r = mid
-        
-        # Expand window from [l-1, r)
-        l, r = l - 1, l
-        
-        while r - l - 1 < k:
-            if l < 0:
-                r += 1
-            elif r >= len(arr):
-                l -= 1
+        l,r = l-1,l
+        while r-l-1 < k:
+            if l<0:
+                r+=1
+            elif r>=len(arr):
+                l-=1
             elif abs(arr[l] - x) <= abs(arr[r] - x):
-                l -= 1
+                l-=1
             else:
-                r += 1
-        
-        return arr[l + 1 : r]
+                r+=1
+        return arr[l+1 : r]
