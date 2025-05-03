60f2defa9adaf366f14be7c67bd49c371a09db96
Avi Tewari <56482375+AviTewari@users.noreply.github.com>
2025-05-02 21:32:33 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (8.25%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,7 +1,6 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        if not strs:
-            return ""
+        
         
         for i in range(len(strs[0])):
             c = strs[0][i]
