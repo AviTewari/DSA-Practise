f75d8f3dcb47af24a3eb4a106c0ec257bd4b0a03
Avi Tewari <56482375+AviTewari@users.noreply.github.com>
2025-05-02 17:21:05 -0700

    Time: 150 ms (20.85%) | Memory: 19 MB (97.72%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -1,41 +1,20 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         count = defaultdict(int)
-        for k,v in enumerate(tasks):
-            count[k]+=1
+        for task in tasks:
+            count[task] += 1
         maxheap = [-cnt for cnt in count.values()]
         heapq.heapify(maxheap)
         q=deque()
         time = 0
         while maxheap or q:
             time += 1
-            if not maxheap:
-                time = q[0][1]
-            else:
+            if  maxheap:
                 cnt = 1 + heapq.heappop(maxheap)
-                q.append([cnt, time+n])
+                if cnt:
+                    q.append([cnt, time+n])
             if q and q[0][1] == time:
                 heapq.heappush(maxheap, q.popleft()[0])
         return time
                 
 
-class Solution:
-    def leastInterval(self, tasks: List[str], n: int) -> int:
-        count = Counter(tasks)
-        maxHeap = [-cnt for cnt in count.values()]
-        heapq.heapify(maxHeap)
-
-        time = 0
-        q = deque()  # pairs of [-cnt, idleTime]
-        while maxHeap or q:
-            time += 1
-
-            if not maxHeap:
-                time = q[0][1]
-            else:
-                cnt = 1 + heapq.heappop(maxHeap)
-                if cnt:
-                    q.append([cnt, time + n])
-            if q and q[0][1] == time:
-                heapq.heappush(maxHeap, q.popleft()[0])
-        return time
